a0c83634c6ce7bca7c937985e44c5fc80c924933
Brandon Orther <brandon@orther.dev>
2020-08-20 00:13:01 -0700

    Add Android SDK paths to zsh env

--- home/pre-compinit.zsh
+++ home/pre-compinit.zsh
@@ -13,3 +13,9 @@ function iterm2_print_user_vars() {
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 eval "$(direnv hook zsh)"
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
